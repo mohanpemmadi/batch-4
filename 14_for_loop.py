6b06b8d8f6664f3a091a5dd56cf656c2338f9689
mohanpemmadi <connectpypi@gmail.com>
2022-05-02 07:53:04 +0530

    loops,break,continue and pass

---
+++ 14_for_loop.py
@@ -0,0 +1,7 @@
+lst = [101,200,300,400]
+
+for value in lst:
+    print('actual value - ',value)
+    res = value/10
+    print('final result -', res)
+    print('@@@@@@@@@@@@@@@@@@@')
---
+++ 15_break_continue_pass.py
@@ -0,0 +1,36 @@
+
+"""
+Break - Used to break the loop
+"""
+'''
+for i in range(10):
+    print(i)
+    if i == 5:
+        break
+'''
+
+'''
+
+value = 10
+while value > 5:
+    print(value)
+    value = value + 1  # value +=1
+    if value == 20:
+        break
+
+'''
+
+""" Continue - Used to skip the iteration """
+
+'''
+for i in range(1,10):
+    if i == 5:
+        continue
+    print(i)
+'''
+
+
+""" pass - used to tell do nothing """
+
+for value in [1,2,'ravi',4]:
+    pass
