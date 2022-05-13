7a8ce68eca6085f8825e0cddbe1f7598c874da32
mohanpemmadi <connectpypi@gmail.com>
2022-05-13 07:45:43 +0530

    oops-2

--- 21_oops.py
+++ 21_oops.py
@@ -61,14 +61,106 @@ class Person:
         print(self.test())
         return 'demo method'
 
-mohan_obj = Person('mohan','pemmadi')
-print(mohan_obj.person_email())
-print(mohan_obj.person_age(31))
-# print(mohan_obj.demo())
-print('@@@@@@@@@@@@')
-venky_obj = Person('venky','abc')
-print(venky_obj.person_email())
-print(venky_obj.person_age(32))
+# mohan_obj = Person('mohan','pemmadi')
+# print(mohan_obj.person_email())
+# print(mohan_obj.person_age(31))
+# # print(mohan_obj.demo())
+# print('@@@@@@@@@@@@')
+# venky_obj = Person('venky','abc')
+# print(venky_obj.person_email())
+# print(venky_obj.person_age(32))
+
+""" Class variable vs Instance variable """
+class Family:
+    sur_name = 'xyz'  # class variable
+
+    def __init__(self,name):
+        self.name = name # instance variable
+
+# obj = Family('abc')
+# obj2 = Family('123')
+
+# accessing variables through object
+
+# print(obj.name)
+# print(obj.sur_name)
+
+# print('################')
+# accessing variables though class name
+
+# print(Family.sur_name)
+# print(Family.name)
+
+# print('******************')
+#
+# print(obj2.sur_name)
+# print(obj2.name)
+
+
+
+""" Inheritance - getting properties from parent class to child class  """
+
+# Single
+
+class SuperClass:
+    def sur_name(self):
+        return 'xyz'
+
+class SubClass(SuperClass):
+    def blood_group(self):
+        return 'B +ve'
+
+# child_obj = SubClass()
+# print(child_obj.blood_group())
+# print(child_obj.sur_name())
+
+# Multi level
+
+class GrandFather:
+    net_worth = 10000000
+    def sur_name(self):
+        return 'xyz'
+
+class Father(GrandFather):
+    def blood_group(self):
+        return 'B +ve'
+
+class Child(Father):
+    def height(self):
+        return '165 cm'
+
+# obj = Child()
+# print(obj.height())
+# print(obj.blood_group())
+# print(obj.sur_name())
+# print(obj.net_worth)
+
+
+# Multiple
+
+class Father:
+    def blood_group(self):
+        return 'B +ve'
+
+class Mother:
+    def blood_group(self):
+        return 'A +ve'
+
+    def color(self):
+        return 'white'
+
+class Son(Father,Mother):
+    def occupation(self):
+        return 'software engineer'
+
+obj = Son()
+print(obj.occupation())
+print(obj.color())
+print(obj.blood_group())
+
+
+
+
 
 
 
