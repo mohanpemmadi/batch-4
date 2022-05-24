52611f1d286581a68a0996b434d773b7385257d5
mohanpemmadi <connectpypi@gmail.com>
2022-05-24 07:41:27 +0530

    database

---
+++ 34_db_connection.py
@@ -0,0 +1,41 @@
+'''
+SQL Server
+Oracle
+PostgreSQL
+MySQL
+
+-----
+
+
+sql
+
+'''
+
+"""
+
+python <-psycopg2->  database(postgresql)
+python <-mysqldb->  database(mysql)
+
+
+"""
+
+'''
+
+install progresql server:
+
+	https://www.enterprisedb.com/downloads/postgres-postgresql-downloads
+	
+guide :
+
+	https://www.guru99.com/download-install-postgresql.html
+
+	
+install psycopg2:
+
+	pip install psycopg2
+
+postgresql tutorial:
+
+	https://www.w3schools.blog/postgresql-tutorial
+		
+'''
